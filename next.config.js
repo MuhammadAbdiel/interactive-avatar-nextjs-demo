5bfa5c63b8da6c42d827c44e965e7771d44f28b8
MuhammadAbdiel <abdielfirdie@gmail.com>
2024-09-24 08:00:59 +0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/static.heygen.ai/static/streaming.proto",
+        destination: "https://static.heygen.ai/static/streaming.proto", // Rewrite it correctly
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
